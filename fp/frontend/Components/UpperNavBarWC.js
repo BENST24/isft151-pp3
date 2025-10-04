3fa1bf40106d2d2c313c9d9f6345ceaad2e4d869
Tomy142 <tomaslema.01@gmail.com>
2025-10-04 12:39:43 -0300

    Add WC modify Controllers

---
+++ fp/frontend/Components/UpperNavBarWC.js
@@ -0,0 +1,79 @@
+class UpperUlWC extends  HTMLElement
+{
+    constructor()
+    {
+        super();
+        const shadow = this.attachShadow({mode: 'open'});
+        const style = document.createElement('style');
+        style.textContent = `
+            .upper-u-list
+            {
+                display: flex;
+                display-direction: row;
+                margin-left: 500px;
+                margin-right: 500px;
+                padding: 20px;
+                width: 40%;
+                margin-top: 40px;
+                background-color: rgba(255, 255, 255, 1);
+                border-radius: 20px;
+            }
+
+            .upper-list
+            {
+                
+                display: flex;
+                flex-direction: row;
+                margin-right:0px;
+                border-color 2px solid black;
+                padding-left: 0px;
+                margin-top: 5px;
+            }
+
+            .upper-a-list
+            {
+                font-size: 20px;
+                text-decoration: none;
+                color: rgba(77, 130, 165, 1);
+            }
+        `;
+        document.body.style.margin = '0';
+        document.body.style.padding = '0';
+        document.body.style.minHeight = '100vh';
+        document.documentElement.style.margin = '0';
+        document.documentElement.style.padding = '0';
+
+        this.uUpperList00 = document.createElement('ul');
+        this.uUpperList00.className = 'upper-u-list';
+
+        this.list00 = document.createElement('li');
+        this.list00.className = 'upper-list';
+        
+        this.aOption00 = document.createElement('a');
+        this.aOption00.href='#';
+        this.aOption00.textContent = 'Gestionar Empleados';
+        this.aOption00.className = 'upper-a-list';
+
+        this.list01 = document.createElement('li');
+        this.list01.className = 'upper-list';
+        
+        this.aOption01 = document.createElement('a');
+        this.aOption01.href='#';
+        this.aOption01.textContent = 'Gestionar Actividades';
+        this.aOption01.className = 'upper-a-list';
+
+        this.list00.appendChild(this.aOption00);
+        this.list01.appendChild(this.aOption01);
+
+    
+        this.uUpperList00.appendChild(this.list00);
+        this.uUpperList00.appendChild(this.list01);
+
+        shadow.appendChild(this.uUpperList00);
+        shadow.appendChild(style);
+    }
+}
+
+customElements.define('u-upper-ul', UpperUlWC);
+
+export{UpperUlWC}
--- fp/frontend/main.js
+++ fp/frontend/main.js
@@ -1,9 +1,7 @@
 import { LoginWC } from "./Components/LoginWC.js";
-import { APIModelAccess } from "../backend/Backend.js";
 
 function main()
 {
-    let model = new APIModelAccess();
     let index = new LoginWC(model);
     document.body.appendChild(index);
 }
