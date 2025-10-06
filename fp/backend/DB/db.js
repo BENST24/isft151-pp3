1211f1725057050e827a7cbbddab6e1aed91b398
Tomy142 <tomaslema.01@gmail.com>
2025-10-06 11:17:23 -0300

    untrack files

--- fp/backend/DB/db.js
+++
@@ -1,9 +0,0 @@
-import mysql from "mysql2/promise";
-
-const db = await mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password:"1234", 
-    database: "isft151-pp3-proyecto",
-});
-export { db };
