a11ee745ece1500d6fe16b6599ea072c61d3fe17
Steck Benjamin <benst642@gmail.com>
2025-10-12 12:48:24 -0300

    Se agregaron las rutas para la gestión de usuarios

--- fp/backend/app.js
+++ fp/backend/app.js
@@ -1,18 +1,25 @@
 import express from "express";
 import cors from "cors";
-import authRoutes from "./Routes/AuthRoutes.js";
+import Routes from "./Routes/Routes.js";
+import rateLimit from "express-rate-limit";
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json()); // Para leer JSON del body
 
-
 app.use(cors());
 
+const globalLimiter = rateLimit({
+    windowMs: 10 * 60 * 1000,
+    max: 200,
+    message: { status: false, result: "TOO_MANY_REQUESTS_GLOBAL" }
+});
+app.use(globalLimiter);
+
 // Montar las rutas
-app.use("/api/auth", authRoutes);
+app.use("/api", Routes);
 
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
