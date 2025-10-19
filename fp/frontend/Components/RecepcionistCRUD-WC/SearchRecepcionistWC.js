cc76b44528f22df0d659a759d3823fbc3106dd3c
Tomy142 <tomaslema.01@gmail.com>
2025-10-19 17:05:36 -0300

    Modify WCs

--- fp/frontend/Components/RecepcionistCRUD-WC/SearchRecepcionistWC.js
+++ fp/frontend/Components/RecepcionistCRUD-WC/SearchRecepcionistWC.js
@@ -32,27 +32,7 @@ class SearchRecepcionistWC extends HTMLElement
                 font-weight: 700;
             }
             
-            .input-search
-            {
-                height: 30px;
-            }
-
-            .span-search
-            {
-                margin: 20px;
-                width: 40px;
-                height: 40px;
-                background-color: rgba(204, 204, 204, 1);
-                color: black;
-            }
-
-            .search-icon
-            {
-                width: 40px;
-                height: 40px;
-                background-color: rgba(204, 204, 204, 1);
-                color: black;
-            }
+            
         `;
         document.body.style.margin = '0';
         document.body.style.padding = '0';
@@ -63,44 +43,22 @@ class SearchRecepcionistWC extends HTMLElement
         this.divMainTitle = document.createElement('div');
         this.divMainTitle.className = 'div-title';
 
-        this.divSearch = document.createElement('div');
-        this.divSearch.className = 'div-search';
-
         this.mainTitle = document.createElement('h2');
         this.mainTitle.className = 'title-main';
         this.mainTitle.textContent = 'Buscar Empleado';
 
-        this.labelSearch = document.createElement('label');
-        this.labelSearch.className = 'label-search';
-
-        this.inputSearch = document.createElement('input');
-        this.inputSearch.className = 'input-search';
-        this.inputSearch.placeholder ='Ingrese el nombre de usuario...';
-
-        this.spanSearch = document.createElement('span');
-        this.spanSearch.className = 'span-search';
-
-        this.searchIcon = document.createElement('img');
-        this.searchIcon.className = 'search-icon';
-        this.searchIcon.src = './assets/buscar.png';
-
-        this.spanSearch.appendChild(this.searchIcon);
-        this.labelSearch.appendChild(this.spanSearch);
-        this.labelSearch.appendChild(this.inputSearch);
-        this.divSearch.appendChild(this.labelSearch);
         this.divMainTitle.appendChild(this.mainTitle);
 
         this.table = new TableWC();
         
         shadow.appendChild(this.divMainTitle);
-        shadow.appendChild(this.divSearch);
         shadow.appendChild(this.table);
         shadow.appendChild(style);
     }
 
     connectedCallback()
     {
-        this.spanSearch.onclick = this.table.loadData.bind(this.table);
+        
     }
 
     disconnectedCallback()
