06d67cd98b507a2c5e4c1e2e5e9b30794a144d1a
Tomy142 <tomaslema.01@gmail.com>
2025-10-30 19:25:00 -0300

    CreateActivity

--- fp/frontend/Controllers/ActivityCRUDControllers/CreateActivityController.js
+++ fp/frontend/Controllers/ActivityCRUDControllers/CreateActivityController.js
@@ -9,12 +9,56 @@
 
         onSaveButtonClick()
         {
-            let username = this.view.inputUser.value;
-            let password = this.view.inputPassword.value;
+            let activityName = this.view.inputActivity.value;
+            let duration = this.view.inputDuration.value;
             let type = this.selectedType;
             let currentUsername = this.view.getAttribute('current-username');
             let currentUserPassword = this.view.getAttribute('current-userpassword');
 
+            if(!activityName)
+            {
+                window.alert('Debe ingresar el nombre de la actividad');
+                return;
+            }
+
+            if(!duration)
+            {
+                window.alert('Debe ingresar la duracion de la actividad');
+                return;
+            }
+
+            if(isNaN(duration) || duration <= 0)
+            {
+                window.alert('La duracion debe ser un numero mayor a 0');
+                return;
+            }
+
+            let requestBody ={
+                currentUsername: currentUsername,
+                currentUserPassword: currentUserPassword,
+                name: activityName,
+                duration: parseInt(duration)
+            };
+
+            fetch('http://localhost:3000/api/activity/create',{
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(requestBody)
+            })
+            .then(function(response){
+                return response.json();
+            })
+            .then(function(result){
+                console.log("Respuesta de la creacion de actividad: ", result);
+                if(result.status){
+                    window.alert("Actividad creada correctamente");
+                    this.onCancelButtonClick();
+                }else{
+                    window.alert('Error: ' + result.result);
+                }
+            }.bind(this))
     
         }
 
